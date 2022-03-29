c6289008cfb17829098f1ab20e75e7cd7365b255
4K05H <rbenakos@gmail.com>
2022-03-29 09:51:02 +0200

    akos

---
+++ kecske.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", init);
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+
+function init(elem) {
+    document.getElementById( "Img" ).addEventListener("click", console.log("krumpli"));
+}
+
+const kepek = [];
+
+
+
